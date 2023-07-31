6f442b5507f740fb9396ec716a09d182a9e04823
Ansible <ansible@localhost.localdomain>
2023-07-31 19:17:22 +0530

    AMI added

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "test-ec2" {
-    name = "vm-test-1"
+    ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
     tags = {
     Name = "ExampleAppServerInstance"
